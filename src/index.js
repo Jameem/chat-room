e4998014cc3d5a2062ebe71af1fe20b6861f1c63
jameem <jameemkurikkal.mp@gmail.com>
2020-02-29 20:36:55 +0530

    first message send

--- src/index.js
+++ src/index.js
@@ -12,6 +12,16 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
+io.on('connection', (socket) => {
+    console.log('New web socket connection')
+
+    socket.emit('message', 'Welcome!!!')
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
+    })
+})
+
 server.listen(port, () => {
     console.log(`Server is up on port ${port}!`)
 })
