72c7e7fbd103656c76c09503dd868daf09688e45
jameem <jameemkurikkal.mp@gmail.com>
2020-03-01 20:40:11 +0530

    User tracking

--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ const {
     generateMessage,
     generateLocationMessage
 } = require('./utils/messages')
+const {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
+} = require('./utils/users')
 
 const app = express()
 const server = http.createServer(app)
@@ -18,17 +24,28 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 
 io.on('connection', (socket) => {
+
     console.log('New web socket connection')
 
-    socket.on('join', ({
-        username,
-        room
-    }) => {
-        socket.join(room)
+    socket.on('join', (options, callback) => {
+        const {
+            error,
+            user
+        } = addUser({
+            id: socket.id,
+            ...options
+        })
+
+        if (error) {
+            return callback(error)
+        }
+
+        socket.join(user.room)
 
         socket.emit('message', generateMessage('Welcome!'))
-        socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined!`))
+        socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined!`))
 
+        callback()
     })
 
     socket.on('sendMessage', (message, callback) => {
@@ -43,7 +60,11 @@ io.on('connection', (socket) => {
     })
 
     socket.on('disconnect', () => {
-        io.emit('message', generateMessage('A user has left!'))
+        const user = removeUser(socket.id)
+
+        if (user) {
+            io.to(user.room).emit('message', generateMessage(`${user.username} has left!`))
+        }
     })
 
     socket.on('sendLocation', (coords, callback) => {
--- src/utils/users.js
+++ src/utils/users.js
@@ -35,14 +35,16 @@ const addUser = ({
     }
 
     users.push(user)
-    return
+    return {
+        user
+    }
 }
 
 const removeUser = (id) => {
     const index = users.findIndex((user) => user.id === id)
 
     if (index !== -1) {
-        return users.splice(index, 1)
+        return users.splice(index, 1)[0]
     }
 }
 
